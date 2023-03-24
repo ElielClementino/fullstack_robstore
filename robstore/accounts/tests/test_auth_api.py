673afd9c5659e49037c4e68d7c824f89c81a8261
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-03-23 23:59:40 -0300

    Adiciona testes

--- robstore/accounts/tests/test_auth_api.py
+++ robstore/accounts/tests/test_auth_api.py
@@ -1,4 +1,4 @@
-from robstore.accounts.models import User
+from robstore.accounts.models import User, Wallet
 from model_bakery import baker
 from . import fixtures
 import json
@@ -105,3 +105,64 @@ def test_user_registering_is_working(client, db):
 
     assert response.status_code == 200
     assert data == {'sucesso': f'usuário { new_user["userInfo"]["username"] }, sua conta foi criada com sucesso '}
+
+
+def test_if_user_wallet_is_being_created(client, db):
+    # Para criar a carteira do usuário é necessário que ele passe pelo service, pois é lá no service que ele define a wallet do user, por isso se criar um user direto por comando não funciona, pois a criação da wallet está atrelada ao service e não ao model.
+    new_user = {"userInfo": { "username": "Liel", "email":"roberto@gmail.com", "password": "Roberto"}}
+    response = client.post("/api/accounts/register", new_user, content_type="application/json")
+
+    user = User.objects.all()
+    wallet = Wallet.objects.all()
+
+    assert response.status_code == 200
+    assert len(wallet) == 1
+    assert wallet[0].user_id == user[0].id
+
+
+def test_if_deposit_with_logged_user_is_working(client, db):
+    # Para criar a carteira do usuário é necessário que ele passe pelo service, pois é lá no service que ele define a wallet do user, por isso se criar um user direto por comando não funciona, pois a criação da wallet está atrelada ao service e não ao model.
+    new_user = {"userInfo": { "username": "Liel", "email":"roberto@gmail.com", "password": "Roberto"}}
+    client.post("/api/accounts/register", new_user, content_type="application/json")
+    client.force_login(User.objects.get(username="Liel"))
+
+    user = User.objects.all()
+    wallet = Wallet.objects.all()
+
+    assert wallet[0].amount_stored == 0.00
+
+    deposit_info = {
+        "user_id": user[0].id,
+        "amount": 2000
+    }
+
+    response = client.post("/api/accounts/wallet-deposit", deposit_info, content_type="application/json")
+
+    assert wallet[0].user_id == user[0].id
+    assert wallet[0].amount_stored == 2000.00
+    assert response.status_code == 200
+
+
+def test_withdraw_money_from_wallet(client, db):
+    # Para criar a carteira do usuário é necessário que ele passe pelo service, pois é lá no service que ele define a wallet do user, por isso se criar um user direto por comando não funciona, pois a criação da wallet está atrelada ao service e não ao model.
+    new_user = {"userInfo": { "username": "Liel", "email":"roberto@gmail.com", "password": "Roberto"}}
+    client.post("/api/accounts/register", new_user, content_type="application/json")
+    client.force_login(User.objects.get(username="Liel"))
+
+    user = User.objects.all()
+    deposit_info = {
+        "user_id": user[0].id,
+        "amount": 2000
+    }
+    response = client.post("/api/accounts/wallet-deposit", deposit_info, content_type="application/json")
+
+    payload = {
+        "user_id":user[0].id,
+        "order_amount": 2000
+    }
+    data = {}
+    response = client.post("/api/accounts/withdraw-wallet-money", payload, content_type="application/json")
+    for resp in response:
+        data = json.loads(resp.decode())
+    
+    assert data['deposited']['new_value'] == '0.00'
